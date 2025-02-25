af7882e559598e81536367b4eb8b47128e842d70
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2025-02-25 17:21:44 +0000

    add: implement dot product and magnitude functions in vector_utils

--- srcs/vector_utils.c
+++ srcs/vector_utils.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   vector_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eamsalem <eamsalem@student.42.fr>          +#+  +:+       +#+        */
+/*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:36:53 by eamsalem          #+#    #+#             */
-/*   Updated: 2025/02/25 15:47:25 by eamsalem         ###   ########.fr       */
+/*   Updated: 2025/02/25 17:20:37 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
+
+double	dot(t_vec3 a, t_vec3 b)
+{
+	return ((a.x * b.x) + (a.y * b.y) + (a.z * b.z));
+}
+
+double	magnatude(t_vec3 a)
+{
+	return (sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z)));
+}
 
 // returns an orthogonal vector to a and b
 t_vec3	cross(t_vec3 a, t_vec3 b)
@@ -27,7 +37,7 @@ t_vec3	normalize(t_vec3 a)
 {
 	double magnitude;
 
-	magnitude = sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
+	magnitude = magnatude(a);
 	a.x /= magnitude;
 	a.y /= magnitude;
 	a.z /= magnitude;
