ec87413dbf652a94e131b55ec295d8d0d7f38047
Eden Judah Amsalem <eamsalem@c1r6s3.42london.com>
2025-03-07 16:53:59 +0000

    organizing code: shadow function split in 2

--- srcs/raytrace/shadow.c
+++ srcs/raytrace/shadow.c
@@ -6,13 +6,13 @@
 /*   By: eamsalem <eamsalem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 11:37:45 by eamsalem          #+#    #+#             */
-/*   Updated: 2025/03/07 16:45:45 by eamsalem         ###   ########.fr       */
+/*   Updated: 2025/03/07 16:52:57 by eamsalem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
 
-bool	shadow_ray_reaches_light(t_ray *ray, void **objs, double light_distance, void *intsec_obj)
+bool	hits_light(t_ray *ray, void **objs, double light_dist, void *intsec_obj)
 {
 	int		i;
     double	t;
@@ -25,34 +25,34 @@ bool	shadow_ray_reaches_light(t_ray *ray, void **objs, double light_distance, vo
 		if (((t_sphere *)objs[i])->shape == SPHERE)
         {
             t = get_sp_t(ray, objs[i]);
-			if (t >= 0 && t < light_distance)
+			if (t >= 0 && t < light_dist)
 				return (false);
         }
 		else if (((t_plane *)objs[i])->shape == PLANE)
 		{
             t = get_pl_t(ray, objs[i]);
-			if (t >= 0 && t < light_distance)
+			if (t >= 0 && t < light_dist)
 				return (false);
 		}
 	}
 	return (true);
 }
 
-void	fire_at_light(t_light *light, t_shadow *shadow, t_intsec *intsec, t_mlx *mlx)
+void	fire_at_light_points(t_light *light, t_shadow *shadow, t_intsec *intsec, t_mlx *mlx)
 {
 	int		i;
 	t_vec3	light_point;
-    double  light_distance;
+    double  light_dist;
 	
 	i = 0;
 	while (i < mlx->consts.shadow_rpp)
 	{
 		light_point = transform_local_to_world(&light->intsec_points[i], &shadow->basis);
 		light_point = add(light_point, light->center);
-		light_distance = magnitude(sub(light_point, shadow->ray.origin));
+		light_dist = magnitude(sub(light_point, shadow->ray.origin));
 		shadow->ray.direction = normalize(sub(light_point, shadow->ray.origin));
 		
-		if (shadow_ray_reaches_light(&shadow->ray, mlx->scene.objs->content, light_distance, intsec->obj))
+		if (hits_light(&shadow->ray, mlx->scene.objs->content, light_dist, intsec->obj))
 		{
 			intsec->in_shadow = false;
 			light->visibility += 1.0 / mlx->consts.shadow_rpp;
@@ -76,7 +76,7 @@ void	cast_shadow_rays(t_intsec *intsec, t_scene *scene, t_mlx *mlx)
 		shadow.ray.direction = light->dir;
 		init_local_basis(&shadow.basis, shadow.ray.direction, &scene->world);
 		gen_rand_light_point(light, &mlx->consts);
-		fire_at_light(light, &shadow, intsec, mlx);
+		fire_at_light_points(light, &shadow, intsec, mlx);
 	}
 	//	printf("light visibility = %f\n", light->visibility);
 }
